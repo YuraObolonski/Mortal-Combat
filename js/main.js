a2aedac693fa51a587d75837182256eed5d695bf
YuraObolonski <55058467+YuraObolonski@users.noreply.github.com>
2021-03-19 18:29:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('#app').vide({
+    mp4: 'video/video.mp4',
+    webm: 'video/video.ogv',
+    ogv: 'video/video.webm',
+    poster: 'images/cover-image.jpg'
+  });
+});
